15e6709ecaf356a2fc0b2d79a80f81b46914926e
Celson Jardim <celsonjardim.dev@outlook.com>
2025-09-02 22:01:44 -0300

    implementa fulxo de alteracao camada API

--- src/main/java/br/com/petz/clientepet/cliente/application/api/ClienteController.java
+++ src/main/java/br/com/petz/clientepet/cliente/application/api/ClienteController.java
@@ -6,6 +6,7 @@ import java.util.UUID;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.petz.clientepet.cliente.application.service.ClienteService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 
@@ -46,5 +47,13 @@ public class ClienteController implements ClienteAPI {
 		log.info("[idCliente] {}", idCliente);
 		clienteService.deletaClienteAtravesId(idCliente);
 		log.info("[finaliza] ClienteController - deletaClienteAtravesId");
+	}
+
+	@Override
+	public void petchAlteraCliente(UUID idCliente, @Valid ClienteAlteracaoRequest clienteRequAlteracaoRequest) {
+		log.info("[inicia] ClienteController - petchAlteraCliente");
+		log.info("[idCliente] {}", idCliente);
+		log.info("[finaliza] ClienteController - petchAlteraCliente");
+		
 	}
 }
