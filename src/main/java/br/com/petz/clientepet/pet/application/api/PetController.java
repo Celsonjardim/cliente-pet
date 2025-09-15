5b5cfc3722c6776bae2e0547b24eaa37b156fd92
Celson Jardim <celsonjardim.dev@outlook.com>
2025-09-15 19:52:18 -0300

    pet petch api corrige retorno para void

--- src/main/java/br/com/petz/clientepet/pet/application/api/PetController.java
+++ src/main/java/br/com/petz/clientepet/pet/application/api/PetController.java
@@ -52,12 +52,11 @@ public class PetController implements PetAPI {
 	}
 
 	@Override
-	public PetAlteracaoRequest patchPet(UUID idCliente, UUID idPet, 
+	public void patchPet(UUID idCliente, UUID idPet, 
 			@Valid PetAlteracaoRequest petAlteracaoRequest) {
 		log.info("[inicia] PetController - patchPet");
 		log.info("[idCliente] {} - [idPet] {}", idCliente, idPet);
 		log.info("[finaliza] PetController - patchPet");
-		return null;
 	}
 
 	
